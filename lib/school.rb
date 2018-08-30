e3012f1f9d64ec51d203014822e56ea037715dd0
samashtar <sam.ashtar@flatironschool.com>
2018-08-30 11:34:46 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+require 'pry'
+class School
+
+attr_accessor :name
+
+def initialize (name)
+  @name = name
+  @roster = {}
+
+end
+
+def roster
+@roster
+end
+
+def add_student (name, grade)
+@name = name
+  @grade = grade
+if @roster.include?(grade) == false
+  @roster[grade] = []
+end
+  @roster[grade] << name
+end
+
+
+def grade (grade)
+  @roster[grade]
+
+end
+
+def sort
+@roster.each do |key,value|
+  @roster[key] = value.sort
+end
+end
+
+
+end
